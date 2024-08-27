cf0baa5c1e463d515832e583bc9771f44258c231
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2024-08-27 12:25:58 +0500

    first

---
+++ user_bot.py
@@ -0,0 +1,29 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from environs import Env
+import asyncio
+
+
+env = Env()
+env.read_env()
+
+api_id = env.str("API_ID")
+api_hash = env.str("API_HASH")
+phone = env.str("PHONE")
+login = "A"
+
+channel_link = -1002149382751
+
+
+bot = Client(name=login, api_id=api_id, api_hash=api_hash, phone_number=phone)
+
+
+@bot.on_message(filters.chat(chats=channel_link))
+async def echo(client: Client, message: Message):
+    if message.from_user is None:
+        await message.reply(text="О, привет )")
+    else:
+        print(f"ID: {message.from_user.id}")
+
+
+asyncio.run(bot.run())
