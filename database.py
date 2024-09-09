348c1a43e3e680caa032b04c99c5bef15e2b26c2
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2024-09-09 21:07:57 +0500

    add sql db

--- database.py
+++ database.py
@@ -13,8 +13,8 @@ class Users:
                 fetchall: bool = False, commit: bool = False):
         if not params:
             params = tuple()
-
-        cursor = self.connection.cursor()
+        connection = self.connection
+        cursor = connection.cursor()
 
         cursor.execute(sql, params)
 
@@ -25,17 +25,36 @@ class Users:
         if fetchall:
             data = cursor.fetchall()
         if commit:
-            self.connection.commit()
+            connection.commit()
 
         return data
 
     def create_table_users(self):
-        sql = "CREATE TABLE IF NOT EXISTS Users(id INT, full_name TEXT, video_code TEXT)"
+        sql = "CREATE TABLE IF NOT EXISTS Users(id INT, full_name TEXT, username TEXT)"
         self.execute(sql, commit=True)
 
-    def insert_data(self, params: tuple=None):
-        sql = "INSERT INTO Users(id, full_name, video_code) VALUES (?, ?, ?)"
-        self.execute()
+    def insert_data(self, params: tuple):
+        sql = "INSERT INTO Users(id, full_name, username) VALUES (?, ?, ?)"
+        self.execute(sql, params=params, commit=True)
+
+    def check_user(self, id):
+        sql = "SELECT * FROM Users WHERE id=?"
+        result = self.execute(sql, params=(id,), fetchone=True)
+        return result
+
+    def get_all_users(self):
+        sql = "SELECT * FROM Users"
+        users = self.execute(sql, fetchall=True)
+
+        user_data = ""
+        for user in users:
+            user_data += f"ID: {user[0]}\nFull Name: {user[1]}\nUsername:{user[2]}\n\n\n"
+
+        # file_path = 'users.txt'
+        # with open(file_path, 'w') as file:
+        #     file.write(user_data)
+
+        return user_data
 
 
 
--- insta.py
+++ insta.py
@@ -2,7 +2,7 @@ from aiogram.utils import exceptions
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
-import asyncio
+from database import Users
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -21,6 +21,12 @@ API_TOKEN = env.str("TOKEN")
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
+db = Users()
+
+
+async def on_start(dp: Dispatcher):
+    db.create_table_users()
+    await dp.bot.send_message(chat_id=5069469905, text="The bot is running !")
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -50,11 +56,13 @@ def get_download_link(instagram_url):
 
 
 async def animate_loading(chat_id, message_id):
-    symbols = ['\\', '|', '/', '\\', '|', '/']
+    symbols = [r'\\', '|', '/', r'\\', '|', '/']
     while True:
         for symbol in symbols:
             try:
-                await bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=f"Загрузка началась, пожалуйста подождите {symbol}")
+                await bot.edit_message_text(chat_id=chat_id, message_id=message_id,
+                                            text=f"Загрузка началась, "
+                                                 f"пожалуйста подождите {symbol}")
                 await asyncio.sleep(0.5)
             except exceptions.MessageNotModified:
                 pass
@@ -62,18 +70,30 @@ async def animate_loading(chat_id, message_id):
 
 @dp.message_handler(regexp=r'https?://(www\.)?instagram\.com/(reel|p)/[^/]+/')
 async def download_reel(message: types.Message):
-    ans = await message.answer("Загрузка началась, пожалуйста подождите ")
+    ans = await message.answer("Загрузка началась, пожалуйста подождите. Максимум 1-2 минуты.")
 
     task = asyncio.create_task(animate_loading(message.chat.id, ans.message_id))
 
     try:
         url = message.text
         download_link = get_download_link(url)
+        if db.check_user(message.from_user.id) is None:
+            if message.from_user.username:
+                parameters = (message.from_user.id, message.from_user.full_name, f"@{message.from_user.username}")
+            else:
+                parameters = (message.from_user.id, message.from_user.full_name, "Nothing")
+            db.insert_data(params=parameters)
+
         await bot.send_video(message.from_user.id, video=download_link)
     finally:
         task.cancel()
         await bot.delete_message(chat_id=message.chat.id, message_id=ans.message_id)
 
 
+@dp.message_handler(commands=["users"])
+async def get_users(message: types.Message):
+    await bot.send_message(chat_id=-1002149382751, text=db.get_all_users())
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_start)
