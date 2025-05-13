6eb4aa5a04b643f866a17d99860ee69e1765a5a0
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2025-05-14 01:31:08 +0500

    in env

--- user_bot.py
+++ user_bot.py
@@ -13,8 +13,10 @@ api_hash = env.str("API_HASH")
 phone = env.str("PHONE")
 login = "A"
 
-channel_link = env.int("CHANNEL_LINK")
-lst = [-1002683245680, -1001725812090, 6287458105, -1001861451231]
+channel_link = env.list("CHANNEL_LINK")
+channel_link = list(map(int, env.list("CHANNEL_LINK")))
+print(channel_link)
+# lst = [-1002683245680, -1001725812090, 6287458105, -1001861451231]
 
 
 bot = Client(name=login, api_id=api_id, api_hash=api_hash, phone_number=phone)
@@ -27,7 +29,7 @@ bot = Client(name=login, api_id=api_id, api_hash=api_hash, phone_number=phone)
 #         await bot.send_message(chat_id=6287458105, text="Comment was written")
 #         await message.reply(text="Люди в шоке")
 
-@bot.on_message(filters.chat(chats=lst))
+@bot.on_message(filters.chat(chats=channel_link))
 async def echo(client: Client, message: Message):
     await message.reply(text="Люди в шоке")
 
